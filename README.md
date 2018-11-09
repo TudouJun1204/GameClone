9af32406fb3681595619a959e5564e454fcf8e4b
TudouJun1204 <37487065+TudouJun1204@users.noreply.github.com>
2018-11-09 18:11:01 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# GameClone
+# Game Clone
+This game contains one player character and enemy bugs. Character needs to reach the water to win. If collision happens between
+character and bugs, player character has to go back to the original point. Player can use left, right, up and down to control
+the character using keyboard arrow keys. When character reach the water without collision, player win.
+
+### Languages
+* HTML5
+* CSS3
+* JavaScript
---
+++ js/app.js
@@ -0,0 +1,136 @@
+var Player = function (x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.handleInput = function (keyPress) {
+
+    if (keyPress == 'left' && this.x > 0) {
+        this.x -= 102;
+    };
+
+    if (keyPress == 'right' && this.x < 405) {
+        this.x += 102;
+    };
+
+    if (keyPress == 'up' && this.y > 0) {
+        this.y -= 83;
+    };
+
+    if (keyPress == 'down' && this.y < 405) {
+        this.y += 83;
+    };
+
+    if (this.y < 0) {
+        setTimeout(function () {
+            this.x = 250;
+            this.y = 500;
+        }, 600);
+
+    };
+};
+
+Player.prototype.update = function() {
+    // Prevent player from moving beyond canvas wall boundaries
+    if (this.y > 380) {
+        this.y = 380;
+    }
+    if (this.x > 400) {
+        this.x = 400;
+    }
+    if (this.x < 0) {
+        this.x = 0;
+    }
+
+    // Check for player reaching top of canvas and winning the game
+    if (this.y < 0) {
+        this.x = 200;
+        this.y = 380;
+    }
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(keyPress) {
+    switch (keyPress) {
+        case 'left':
+            this.x -= this.speed + 50;
+            break;
+        case 'up':
+            this.y -= this.speed + 30;
+            break;
+        case 'right':
+            this.x += this.speed + 50;
+            break;
+        case 'down':
+            this.y += this.speed + 30;
+            break;
+    }
+};
+
+var player = new Player(200, 380, 50);
+
+
+// Enemies our player must avoid
+var Enemy = function (x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = 'images/enemy-bug.png';
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function (dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    this.x += this.speed * dt;
+    //reset enemy movement when off canvas
+    if (this.x > 505){
+      this.x = -50;
+      this.speed = 200 + Math.floor(Math.random() * 300);
+    }
+    if (player.x < this.x + 80 &&
+        player.x + 30 > this.x &&
+        player.y < this.y + 25 &&
+        player.y + 25 > this.y ){
+          player.x = 200;
+          player.y = 380;
+        }
+};
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+var allEnemies = [];
+var enemyLocation = [60, 140, 220];
+var enemy
+
+
+enemyLocation.forEach(function(locationY){
+  enemy = new Enemy(0, locationY, 200);
+  allEnemies.push(enemy);
+});
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function (e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
